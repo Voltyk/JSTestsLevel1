c01efcb5b4bb5a3ac8c7d92f1c1eb6aece53f031
Voltyk <v.malega@gmail.com>
2018-10-17 15:53:45 +0300

    Update src/JS_4th_task.js

--- src/JS_4th_task.js
+++ src/JS_4th_task.js
@@ -15,14 +15,115 @@ class Hero {
     this.className=className;
   }
   get className(){
-    return this._className;
+    return this.charClass;
   }
   set className(value){
-    if (value==='Warrior') {
-      this._className='Warrior';
-    }else {
-      this._className='Incorrect character class provided';
+    this.charClass='Incorrect character class provided';
+    for(var prop in Hero.heroClasses){
+      if (Hero.heroClasses[prop]['charClass']===value) {
+        this.charClass=Hero.heroClasses[prop]['charClass'];
+        this.life=Hero.heroClasses[prop]['life'];
+        this.damage=Hero.heroClasses[prop]['damage'];
+       break;
+      }
     }
   }
+  getName(){
+    return this.name;
+  }
+  getCharClass(){
+    return this.charClass;
+  }
+  attack(target){
+
+  }
 }
-hero1=new Hero('Vova','Trol');
+Hero.heroClasses = {
+	warrior: {
+		charClass: "Warrior",
+		life: 30,
+		damage: 4
+	},
+	rogue: {
+		charClass: "Rogue",
+		life: 25,
+		damage: 3
+	},
+	sorcerer: {
+		charClass: "Sorcerer",
+		life: 20,
+		damage: 5
+	}
+}
+
+class Monster {
+  constructor(monsterClass) {
+    this.monsterClass=monsterClass;
+  }
+  get monsterClass(){
+    return this.charClass;
+  }
+  set monsterClass(value){
+    this.charClass='Incorrect character class provided';
+    for(var prop in Monster.monsterClasses){
+      if (Monster.monsterClasses[prop]['charClass']===value) {
+        this.charClass=Monster.monsterClasses[prop]['charClass'];
+        this.life=Monster.monsterClasses[prop]['life'];
+        this.damage=Monster.monsterClasses[prop]['damage'];
+       break;
+      }
+    }
+  }
+  getName(){
+    return this.name;
+  }
+  getCharClass(){
+    return this.charClass;
+  }
+  attack(target){
+
+  }
+}
+Monster.monsterClasses = {
+	zombie: {
+		charClass: "Zombie",
+		life: 8,
+		damage: 4
+	},
+	skeleton: {
+		charClass: "Skeleton",
+		life: 10,
+		damage: 6
+	},
+	holem: {
+		charClass: "Holem",
+		life: 15,
+		damage: 6
+	}
+}
+class Game {
+  constructor(status='',hero={},monsters=[]) {
+    this.status=status;
+    this.hero=hero;
+    this.monsters=monsters;
+  }
+  beginJourney(){}
+  finishJourney(){}
+  addHero(hero){
+    if (!hero instanceof Hero) {
+      console.log('Only hero instance can be hero');
+    } else if (this.hero instanceof Hero) {
+      console.log('Only one hero can exist');
+    }else {
+      this.hero=hero;
+      console.log(Hero created, welcome ${hero.name});
+    }
+
+
+  }
+  addMonster(monster){
+
+  }
+  fight(){}
+
+}
