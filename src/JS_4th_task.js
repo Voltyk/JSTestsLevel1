580d837f1bbae0167a9b3d83bec0e99c0f3552be
Voltyk <v.malega@gmail.com>
2018-10-18 08:01:31 +0300

    Update src/JS_4th_task.js

--- src/JS_4th_task.js
+++ src/JS_4th_task.js
@@ -8,9 +8,32 @@
 // В случае создания персонажа несуществующего класса -
 // вернуть исключение "Incorrect character class provided"
 // свойства и методы героев и монстров так же указыны в спецификации
+class Character {
+  constructor() {
 
-class Hero {
+  }
+  getName(){
+    return this.name;
+  }
+  getCharClass(){
+    return this.charClass;
+  }
+  attack(target){
+    if ((target instanceof Hero) || (target instanceof Monster)) {
+      target.life-=this.damage;
+      if (target.life<=0) {
+        return `${target.charClass} killed`
+      } else {
+        return `done ${this.damage} damage to ${target.charClass}`
+      }
+    } else {
+      return 'monster can not attack monster or hero attack hero';
+    }
+  }
+}
+class Hero extends Character {
   constructor(name,className) {
+    super();
     this.name=name;
     this.className=className;
   }
@@ -24,18 +47,18 @@ class Hero {
         this.charClass=Hero.heroClasses[prop]['charClass'];
         this.life=Hero.heroClasses[prop]['life'];
         this.damage=Hero.heroClasses[prop]['damage'];
-       break;
+        break;
       }
     }
   }
-  getName(){
-    return this.name;
-  }
-  getCharClass(){
-    return this.charClass;
-  }
-  attack(target){
 
+  attack(target){
+    if(!(target instanceof Monster)){
+      return 'I will attack only monsters';
+    }else {
+      let msg=Character.prototype.attack.call(this,target);
+      return `Hero attacked, ${msg}`;
+    }
   }
 }
 Hero.heroClasses = {
@@ -56,8 +79,9 @@ Hero.heroClasses = {
 	}
 }
 
-class Monster {
+class Monster extends Character {
   constructor(monsterClass) {
+    super();
     this.monsterClass=monsterClass;
   }
   get monsterClass(){
@@ -70,18 +94,18 @@ class Monster {
         this.charClass=Monster.monsterClasses[prop]['charClass'];
         this.life=Monster.monsterClasses[prop]['life'];
         this.damage=Monster.monsterClasses[prop]['damage'];
-       break;
+        break;
       }
     }
   }
-  getName(){
-    return this.name;
-  }
-  getCharClass(){
-    return this.charClass;
-  }
   attack(target){
+    if(!(target instanceof Hero)){
+      return 'I will attack only heroes';
 
+    }else {
+      let msg=Character.prototype.attack.call(this,target);
+      return `Monster attacked, ${msg}`;
+    }
   }
 }
 Monster.monsterClasses = {
@@ -102,28 +126,73 @@ Monster.monsterClasses = {
 	}
 }
 class Game {
-  constructor(status='',hero={},monsters=[]) {
+  constructor(status='Idle',hero={},monsters=[]) {
     this.status=status;
     this.hero=hero;
     this.monsters=monsters;
   }
-  beginJourney(){}
-  finishJourney(){}
+  beginJourney(){
+    if (this.hero!=={} && this.monsters!==[]) {
+      this.status='In progress'
+      console.log('Your journey has started, fight monsters');
+    } else {
+      console.log('Cannot start journey, populate the world with hero and monsters first');
+    }
+  }
+  finishJourney(){
+    let monstsAlive=this.monsters.some((item)=>(item['life']>0))
+    if (this.hero.life<=0 || !monstsAlive) {
+      this.status='Finished';
+      if (!monstsAlive) {
+        console.log('The Game is finished. Monstrs are dead. Congratulations');
+      }
+      if (this.hero.life<=0) {
+        console.log('The Game is finished. Hero is dead :(');
+      }
+    }else {
+      console.log('Don`t stop. Some monsters are still alive. Kill`em all');
+    }
+  }
   addHero(hero){
-    if (!hero instanceof Hero) {
+    if (!(hero instanceof Hero)) {
       console.log('Only hero instance can be hero');
     } else if (this.hero instanceof Hero) {
       console.log('Only one hero can exist');
     }else {
       this.hero=hero;
-      console.log(Hero created, welcome ${hero.name});
+      console.log(`Hero created, welcome ${hero.name}`);
     }
 
 
   }
   addMonster(monster){
-
+    if (!(monster instanceof Monster)) {
+      console.log('Only monster Instances can become monsters');
+    } else if (this.monsters.length>=Game.maxMonsters) {
+      console.log('Only 2 monsters can exist');
+    }else {
+      if (this.monsters.some((item)=>(item===monster))) {
+        console.log('This monster already exists. Add another monster instance');
+      } else {
+        this.monsters.push(monster);
+        console.log(`Monster Created, ${monster.charClass} appeared in the world`);
+      }
+    }
   }
-  fight(){}
+  fight(monsterIndex){
+    while(true){
+      console.log(this.hero.attack(this.monsters[monsterIndex]));
+      if (this.monsters[monsterIndex].life<=0) {
+        return 'Hero win';
+        break;           //return should break all the method including current loop, but for shure...
+      }
+      console.log(this.monsters[monsterIndex].attack(this.hero));
+      if (this.hero.life<=0) {
+        return 'Monster win';
+        break;
+      }
+    }
+  }
 
 }
+Game.maxMonsters = 2;
